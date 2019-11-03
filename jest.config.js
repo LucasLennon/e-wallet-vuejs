5439437054b350661bfb895a515fbad381a53a86
unknown <lucas.esperidiao@snowmanlabs.com>
2019-11-03 04:16:31 -0300

    Improve jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
-}
+  preset: "@vue/cli-plugin-unit-jest",
+  moduleFileExtensions: ["js", "jsx"],
+  moduleDirectories: ["node_modules"],
+  testMatch: ["**/*.spec.[jt]s"]
+};
