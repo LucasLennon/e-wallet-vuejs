5f6a12266fef6e0fefd844ba62ef1083560bef92
unknown <lucas.esperidiao@snowmanlabs.com>
2019-11-07 02:31:57 -0300

    Add vue-currency-input

---
+++ src/core/plugins/vue-currency-input.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import VueCurrencyInput from 'vue-currency-input'
+
+export default Vue.use(VueCurrencyInput)
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './core/router'
 import store from './core/store'
 import vuetify from './core/plugins/vuetify';
 import './core/plugins/vue-the-mask';
+import './core/plugins/vue-currency-input';
 
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
