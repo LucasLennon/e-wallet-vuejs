47e1cfe17b1f936cee11657cadb97e472e693062
unknown <lucas.esperidiao@snowmanlabs.com>
2019-11-06 23:55:22 -0300

    Improvement on Login page and Store

--- src/modules/LoginAndRegistration/store/index.js
+++ src/modules/LoginAndRegistration/store/index.js
@@ -11,16 +11,24 @@ export default {
     }
   },
   actions: {
-    async requestLogin({commit}, payload){
+    async requestLogin(_, payload){
       await LocalApi.accessDB()
       const { data } = await LocalApi.login(payload);
+      localStorage.setItem("token", data.token);
+      return data;
+    },
+    async requestUser({commit}){
+      const token = localStorage.getItem("token")
+      await LocalApi.accessDB()
+      const { data } = await LocalApi.getUser(token);
       commit("SET_USER", data);
       return data;
     },
     async requestLogout({commit}){
       commit("SET_USER", null);
+      localStorage.removeItem("token");
     },
-    async registerNewUser({ commit }, payload) {
+    async registerNewUser(_, payload) {
       await LocalApi.accessDB();
       try {
         const response = await LocalApi.addToUsers(payload);
