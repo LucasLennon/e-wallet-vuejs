397603a24a83ca5c6c383700f1e5aad949554dda
unknown <lucas.esperidiao@snowmanlabs.com>
2019-11-09 19:01:08 -0300

    Improving store for the divided local api

--- src/modules/LoginAndRegistration/store/index.js
+++ src/modules/LoginAndRegistration/store/index.js
@@ -1,34 +1,34 @@
 // import LocalApi from "@/core/services/LocalApi";
 import LocalUser from "@/core/services/LocalUser";
-// import LocalTransaction from "@/core/services/LocalTransaction";
+import LocalTransaction from "@/core/services/LocalTransaction";
 import LocalToken from "@/core/services/LocalToken";
 
 export default {
   namespaced: true,
   state:{
+    userCurrencyList: null,
     user: null,
   },
   mutations: {
+    SET_USER_CURRENCY(state, payload){
+      state.userCurrencyList = payload;
+    },
     SET_USER(state, payload){
       state.user = payload;
     }
   },
   actions: {
     async requestLogin(_, payload){
-      try {
-        const { data } = await LocalToken.login(payload);
-        localStorage.setItem("token", data.token);
-        return data;
-      } catch (error) {
-        console.error(error);
-        
-      }
+      const { data } = await LocalToken.login(payload);
+      localStorage.setItem("token", data.token);
+      return data;
     },
     async requestUser({commit}){
       const token = localStorage.getItem("token")
       // await LocalAPI.accessDB();
       const { data } = await LocalUser.getUser(token);
       commit("SET_USER", data);
+      commit("SET_USER_CURRENCY", data.currency);
       return data;
     },
     async requestLogout({commit}){
