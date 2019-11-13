fb0819cfc6bc359b8cca37a7dec341b767fa947e
unknown <lucas.esperidiao@snowmanlabs.com>
2019-11-13 02:55:37 -0300

    Create test for Exchange Page

--- src/modules/LoginAndRegistration/store/index.js
+++ src/modules/LoginAndRegistration/store/index.js
@@ -5,33 +5,47 @@ import LocalToken from "@/core/services/LocalToken";
 
 export default {
   namespaced: true,
-  state:{
+  state: {
     userCurrencyList: null,
-    user: null,
+    user: {
+      cpf: "",
+      name: "",
+      email: "",
+      image: "",
+      password: "",
+      id: "",
+      currency: [
+        {
+          simbolo: "BRL",
+          nomeFormatado: "",
+          tipoMoeda: "",
+          quantity: 1
+        }
+      ]
+    }
   },
   mutations: {
-    SET_USER_CURRENCY(state, payload){
+    SET_USER_CURRENCY(state, payload) {
       state.userCurrencyList = payload;
     },
-    SET_USER(state, payload){
+    SET_USER(state, payload) {
       state.user = payload;
     }
   },
   actions: {
-    async requestLogin(_, payload){
+    async requestLogin(_, payload) {
       const { data } = await LocalToken.login(payload);
       localStorage.setItem("token", data.token);
       return data;
     },
-    async requestUser({commit}){
-      const token = localStorage.getItem("token")
-      // await LocalAPI.accessDB();
+    async requestUser({ commit }) {
+      const token = localStorage.getItem("token");
       const { data } = await LocalUser.getUser(token);
       commit("SET_USER", data);
       commit("SET_USER_CURRENCY", data.currency);
-      return data;
+      return true;
     },
-    async requestLogout({commit}){
+    async requestLogout({ commit }) {
       commit("SET_USER", null);
       localStorage.removeItem("token");
     },
